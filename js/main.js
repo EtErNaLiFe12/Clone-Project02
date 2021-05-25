4ac55aa50fd10c850151369513ec406600467481
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-05-25 21:37:55 +0900

    클래스 add/remove 이벤트 추가(임시)

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Mouseover시 class 추가 이벤트(임시-수정해야함)
+
+const displayEl = document.getElementsByClassName('price');
+for (let i = 0; i < displayEl.length; i++) {
+    displayEl[i].addEventListener('mouseover', function(){
+	    this.classList.add("displayed");
+    })
+    displayEl[i].addEventListener('mouseout', function(){
+	    this.classList.remove("displayed");
+    })
+}
+// const infoEls = document.querySelectorAll('.info');
+// const descriptionEls = document.querySelectorAll('.title');
+// let fadeOut = true;
+// descriptionEls.addEventListener('click', function () {
+//   fadeOut = !fadeOut
+//   if (fadeOut) {
+//     //Class 추가
+//     infoEls.classList.add('fadeout');
+//   } else {
+//     //원상 복귀
+//     infoEls.classList.remove('fadeout');
+//   }
+// });
